c8660604a49fd09c6df49494289ac7bf3ae64f79
nessaoana <nessaoana09@gmail.com>
2025-12-02 17:59:26 -0300

    Refactor Terraform configuration: add Datadog integration variables and update Lambda function settings

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,3 +1,10 @@
+locals {
+  datadog_layers = concat(
+    var.datadog_layer_arn != "" ? [var.datadog_layer_arn] : [],
+    var.datadog_extension_arn != "" ? [var.datadog_extension_arn] : []
+  )
+}
+
 resource "aws_lambda_function" "auth_lambda" {
   s3_bucket     = var.lambda_s3_bucket
   s3_key        = var.lambda_s3_key
@@ -5,6 +12,21 @@ resource "aws_lambda_function" "auth_lambda" {
   handler       = "handler.lambda_handler"
   runtime       = "python3.12"
   role          = aws_iam_role.lambda_role.arn
+
+  # Attach Datadog layers if provided
+  layers = local.datadog_layers
+
+  environment {
+    variables = merge(
+      {
+        # Basic metadata
+        DD_ENV   = var.environment
+        DD_SERVICE = "fiap-auth-lambda"
+        DD_TRACE_ENABLED = "true"
+      },
+      var.datadog_api_key != "" ? { DD_API_KEY = var.datadog_api_key } : {}
+    )
+  }
 
   timeout     = 10
   memory_size = 128
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,3 @@
 output "lambda_function_name" {
   value = aws_lambda_function.auth_lambda.function_name
 }
-
-output "lambda_bucket_name" {
-  value = aws_s3_bucket.lambda_code.bucket
-}
