db121b266cd00a5ccdb15fd1828dd7cc9267ac38
nessaoana <nessaoana09@gmail.com>
2025-12-07 23:01:32 -0300

    Refactor lambda function to use Datadog module for monitoring and tracing

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -5,28 +5,27 @@ locals {
   )
 }
 
-resource "aws_lambda_function" "auth_lambda" {
+module "lambda-datadog" {
+  source  = "DataDog/lambda-datadog/aws"
+  version = "4.0.0"
+
+  environment_variables = {
+    "DD_API_KEY" : var.datadog_api_key
+    "DD_ENV" : var.environment
+    "DD_SERVICE" : "fiap-auth-lambda"
+    "DD_SITE": "https://us5.datadoghq.com/"
+    "DD_TRACE_ENABLED" : "true"
+  }
+
+  datadog_extension_layer_version = 86
+  datadog_python_layer_version = 119
+
   s3_bucket     = var.lambda_s3_bucket
   s3_key        = var.lambda_s3_key
   function_name = "fiap-auth-lambda"
   handler       = "handler.lambda_handler"
   runtime       = "python3.12"
   role          = aws_iam_role.lambda_role.arn
-
-  # Attach Datadog layers if provided
-  layers = local.datadog_layers
-
-  environment {
-    variables = merge(
-      {
-        # Basic metadata
-        DD_ENV           = var.environment
-        DD_SERVICE       = "fiap-auth-lambda"
-        DD_TRACE_ENABLED = "true"
-      },
-      var.datadog_api_key != "" ? { DD_API_KEY = var.datadog_api_key } : {}
-    )
-  }
 
   timeout     = 10
   memory_size = 128
