844281f272e09a877d009a10a15494613f21bfc4
isaahs <isaahs41@gmail.com>
2025-12-13 15:38:31 -0300

    feat: adicionando ClienteModel

---
+++ app/src/model/cliente.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Boolean, Integer
+
+from src.utils.conexao_db import Base
+
+
+class ClienteModel(Base):
+    __tablename__ = 'clientes'
+
+    id = Column(Integer, primary_key=True)
+    cpf = Column(String, unique=True, nullable=False)
+    nome = Column(String, nullable=False)
+    email = Column(String)
+    ativo = Column(Boolean, default=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "cpf": self.cpf,
+            "nome": self.nome,
+            "email": self.email,
+            "ativo": self.ativo
+        }
