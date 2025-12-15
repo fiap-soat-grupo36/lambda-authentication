919e11dda2c7f35fc3819f8086ff45aeb66f3a8b
isaahs <isaahs41@gmail.com>
2025-12-14 21:44:49 -0300

    feat: adicionar configuração de aplicação e inicialização do gerenciador de banco de dados

---
+++ app/src/utils/config.py
@@ -0,0 +1,16 @@
+import os
+
+from src.utils.conexao_db import GerenciadorDB
+
+
+class AppConfig:
+    DB_SECRET_NAME = os.environ.get("DB_SECRET_NAME")
+    JWT_SECRET_NAME = os.environ.get("JWT_SECRET_NAME")
+    AWS_REGION = os.environ.get("AWS_REGION", "sa-east-1")
+
+
+def inicializar_aplicacao():
+    GerenciadorDB.inicializar(
+        secret_name=AppConfig.DB_SECRET_NAME,
+        region=AppConfig.AWS_REGION
+    )
