58bddef224fce5f653b9b58f6814089307a7ce76
nessaoana <nessaoana09@gmail.com>
2025-12-15 10:14:58 -0300

    fix: add missing key configuration in backend.tf for S3 backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket               = "projeto-oficina-terraform"
+    key                  = "terraform.tfstate"
     workspace_key_prefix = "lambda"
     region               = "us-east-2"
   }
