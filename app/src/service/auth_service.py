955328a032e2d9b6a89dce1d748ed592feda9d58
nessaoana <nessaoana09@gmail.com>
2025-12-22 20:49:04 -0300

    feat: implement RDS connection and logging enhancements for Lambda authentication

--- app/src/service/auth_service.py
+++ app/src/service/auth_service.py
@@ -4,6 +4,7 @@ from src.exception.validacoes_exception import ClienteInativoError
 from src.repository.cliente_repository import ClientRepository
 from src.service.token_service import GeradorTokenJWT
 from src.service.validador_cpf_service import ValidadorCPFService
+from src.utils.config import logger
 
 
 class AuthService:
@@ -14,13 +15,21 @@ class AuthService:
         region: str,
         expira_em_minutos: int = 30,
     ) -> Dict[str, str]:
+        logger.debug('Validando CPF')
         cpf_limpo = ValidadorCPFService.validar_cpf(cpf)
 
+        logger.debug('Buscando cliente no banco de dados')
         cliente = ClientRepository.buscar_por_cpf(cpf_limpo)
 
         if not cliente.ativo:
+            logger.warning(
+                f'Cliente inativo', extra={'cliente_id': str(cliente.id)}
+            )
             raise ClienteInativoError('Cliente está inativo.')
 
+        logger.debug(
+            'Gerando token JWT', extra={'cliente_id': str(cliente.id)}
+        )
         token = GeradorTokenJWT.gerar_token(
             cpf=cliente.cpf,
             cliente_id=str(cliente.id),
@@ -31,4 +40,7 @@ class AuthService:
             expira_em_minutos=expira_em_minutos,
         )
 
+        logger.info(
+            'Token gerado com sucesso', extra={'cliente_id': str(cliente.id)}
+        )
         return {'access_token': token, 'token_type': 'Bearer'}
